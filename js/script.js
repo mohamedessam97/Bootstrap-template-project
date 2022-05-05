799515b43caec428af9d075e3d787274f12ed1af
Mohamed Essam <mohamedessam9798@gmail.com>
2022-05-05 19:45:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var btnbar =document.getElementById("btnbar");
+
+// btnbar.addEventListener("click",function(){
+//     this.style.backgroundColor="#ffb600";
+// })
+
+function back(e){
+    e.target.classList.toggle("mystyle");
+    // e.target.style.padding="10px 15px 5px 15px";
+    // e.target.style.backgroundColor="#ffb600";
+}
